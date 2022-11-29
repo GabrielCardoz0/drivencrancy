755891b894094a559b856b19ad55aee38e4ea2fe
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2022-11-29 14:32:56 -0300

    feat:vote schema

--- src/models/schemas.js
+++ src/models/schemas.js
@@ -4,4 +4,9 @@ import Joi from "joi";
 export const newPollSchema = Joi.object({
     title:Joi.string().required(),
     expireAt:Joi.required()
-}) 
+});
+
+export const voteSchema = Joi.object({
+    title: Joi.string().required(),
+	pollId: Joi.string().required()
+});
