47b6e9aaadfa6291abe9f19bbf282e299e77691f
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2022-11-30 16:24:30 -0300

    feat: connect database online

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import express from "express"
-import cors from "cors"
-import router from "./routes/router.js"
+import express from "express";
+import cors from "cors";
+import router from "./routes/router.js";
+import dotenv from "dotenv";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(router)
+dotenv.config();
 
-app.listen(5000)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
+const port = process.env.PORT || 5000;
 
-//metodo delete para apagar inputs:
-// iniciar DB: mongod --dbpath ~/.mongo
-//iniciar servidor local: npx nodemon src/index.js
-//iniciar terminal mongo: mongo
-//rodar o index: node src/index.js
-// rodar projeto react: npm start
+app.listen(port, ()=> console.log(`Server running in port: ${port}`));
