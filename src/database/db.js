00b9efdc36bedb8c46d2a2b2b9216c514da76be6
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2022-11-30 15:24:48 -0300

    feat: dotenv e variavel global MONGO_URI

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,9 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 await mongoClient.connect().then(() => {
